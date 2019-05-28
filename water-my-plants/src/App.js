baf2545d42a944b0e0f3236c662143d74fcc918d
JordanG <jmg236@yahoo.com>
2019-05-28 14:06:24 -0500

    created react app

---
+++ water-my-plants/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <AppContainer>
+        
+      </AppContainer>
+    );
+  }
+}
+
+
+export default App;
